dac022e67ba0d0911632124fbb7575edf1b62e48
Eran Pietsch <eranpietsch@gmail.com>
2017-05-17 09:15:37 +0800

    Combined summary pipeline working with attractiveness and support.

--- imports/api/temp-data/temp-pipelines/combined-summary-pipeline/combinedSummaryPipeline.js
+++ imports/api/temp-data/temp-pipelines/combined-summary-pipeline/combinedSummaryPipeline.js
@@ -17,7 +17,8 @@ export const combinedSummaryPipeline = [
       secondaryDomain: 1,
       controllingActorName: 1,
       controllingActivityName: 1,
-      controllingValueName: 1, 
+      controllingValueName: 1,
+      scoreClass: 1, 
       category: {
         $cond: [
           {
@@ -49,9 +50,46 @@ export const combinedSummaryPipeline = [
     }
   },
   {
-    $limit: 100
+    $group: {
+      _id: "$category",
+      totalCategoryCount: {
+        $sum: 1
+      },
+      low: {
+        $sum: {
+            $cond: [{$eq: ["$scoreClass", "low"]}, 1, 0 ]
+          }
+      },
+      medium: {
+          $sum: {
+          $cond: [{$eq: ["$scoreClass", "medium"]}, 1, 0]
+        }
+      },
+      high: {
+          $sum: {
+          $cond: [{$eq: ["$scoreClass", "high"]}, 1, 0]
+        }
+      },
+    }
   },
   {
-    $sort: {primaryName: 1, secondaryName: 1}
-  }
+    $project: {
+      lowImpactPercentage: {
+        $multiply: [{$divide: ["$low", "$totalCategoryCount"]}, 100]
+      },
+      mediumImpactPercentage: {
+        $multiply: [{$divide: ["$medium", "$totalCategoryCount"]}, 100]
+      },
+      highImpactPercentage: {
+        $multiply: [{$divide: ["$high", "$totalCategoryCount"]}, 100]
+      },
+
+    }
+  },
+  // {
+  //   $limit: 100
+  // },
+  // {
+  //   $sort: {primaryName: 1, secondaryName: 1}
+  // }
 ]
