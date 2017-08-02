9997c3ab3f342167621c63539c3c7403462e96ed
Eran Pietsch <eranpietsch@gmail.com>
2017-08-02 10:33:37 +0800

    Adding menu data item now correctly handles empty collection.

--- imports/api/server/server-functions/menu-server-functions/findLastToplevelMenuItemDstring.js
+++ imports/api/server/server-functions/menu-server-functions/findLastToplevelMenuItemDstring.js
@@ -4,16 +4,18 @@ import { MENUDATAITEMS } from '../../../collections/menuCollections.js';
 export const findLastTopLevelMenuDataItemDstring = function() {
 
 	let lastDstring;
+	const menuDataItemsLength = MENUDATAITEMS.find({}).count();
+	console.log(menuDataItemsLength);
 
-	if(!MENUDATAITEMS) {
+	if(menuDataItemsLength === 0) {
 		console.log("no menu data items");
-		lastDstring = "1";
+		lastDstring = "0";
 	}else {
 		const lastDstringPipeline = [		
 			{$match: {staticIndentLevel: "0"}},
 			{$sort: {staticSortString: 1}},
 			{$group: {_id: null, last: {$last: "$$ROOT"}}},		
-	]
+		]
 
 		const topLevelArray=MENUDATAITEMS.aggregate(lastDstringPipeline);
 		console.log(topLevelArray);
@@ -22,7 +24,7 @@ export const findLastTopLevelMenuDataItemDstring = function() {
 		if(topLevelArray) {
 			lastDstring = topLevelArray[0].last.staticDstring;
 		}else {
-			lastDstring = 1;
+			lastDstring = "0";
 		}
 	}
 	console.log("last dstring", lastDstring);
