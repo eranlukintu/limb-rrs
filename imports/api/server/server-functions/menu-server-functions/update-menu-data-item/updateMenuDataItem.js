da2f2ece27d267bb91964f18c7c356673a2440fc
Eran Pietsch <eranpietsch@gmail.com>
2017-10-10 12:55:51 +0800

    Shifted logic of update menu data item to separate function rather than being contained in method.

---
+++ imports/api/server/server-functions/menu-server-functions/update-menu-data-item/updateMenuDataItem.js
@@ -0,0 +1,39 @@
+import { MENUDATAITEMS } from '../../../../collections/menuCollections.js';
+import { MENUDATAROWS } from '../../../../collections/menuCollections.js';
+import { getHeadDrow } from '../../dRow-functions/getHeadDrow.js';
+import { createInitialArgs } from '../update-menu-data-row/createInitialArgs.js';
+import { attributePhrases } from '../../server-fixtures/attributePhrases.js';
+import { createAttributeHierarchyArray } from '../../dRow-functions/createAttributeHierarchyArray.js';
+import { getDirectChildren } from '../../dRow-functions/getDirectChildren.js';
+import { createMenuDataRowsArray } from '../..//dRow-functions/createMenuDataRowsArray';
+import { getAttributeDrows } from '../../dRow-functions/getAttributeDrows.js';
+import { createCategorisedDataRowsArray } from '../update-menu-data-row/createCategorisedDataRowsArray.js';
+import { saveMenuDataRows } from '../update-menu-data-row/saveMenuDataRows.js';
+import { getPendingAttributeDrows } from '../../dRow-functions/getPendingAttributeDrows.js';
+import { getLastDescriptionWord } from '../../dRow-functions/getLastDescriptionWord.js';
+import { updatePendingAttributeDrows } from '../../dRow-functions/updatePendingAttributeDrows.js';
+
+export const updateMenuDataItem = function(menuDataItem) {
+		
+	const sourceDrowId = menuDataItem.sourceDrowId;
+	// console.log("update started");
+
+	createInitialArgs(MENUDATAITEMS, attributePhrases, getDirectChildren, MENUDATAROWS)
+	.then(function(args) {
+		// console.log(args);
+		return getHeadDrow(args, sourceDrowId, menuDataItem);
+	})
+	.then(function(args) {
+		// console.log(args.originalHeadDrow);
+		return getAttributeDrows(args);
+	})
+	.then(function(args) {
+		return getPendingAttributeDrows(args, getLastDescriptionWord);
+	})
+	.then(function(args) {
+		updatePendingAttributeDrows(args, MENUDATAITEMS);
+	})
+	.catch(function(err) {
+		console.log(err);
+	});
+}
--- imports/api/server/server-methods/updateMenuDataItemMethod.js
+++ imports/api/server/server-methods/updateMenuDataItemMethod.js
@@ -1,22 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
-import { MENUDATAITEMS } from '../../collections/menuCollections.js';
-import { MENUDATAROWS } from '../../collections/menuCollections.js';
-import { getHeadDrow } from '../server-functions/dRow-functions/getHeadDrow.js';
-// import { createMenuDataItemsArray } from '../server-functions/menu-server-functions/update-menu-data-item/createMenuDataItemsArray.js';
-// import { getHeadDrows } from '../server-functions/dRow-functions/getHeadDrows.js';
-import { createInitialArgs } from '../server-functions/menu-server-functions/update-menu-data-row/createInitialArgs.js';
-import { attributePhrases } from '../server-functions/server-fixtures/attributePhrases.js';
-import { createAttributeHierarchyArray } from '../server-functions/dRow-functions/createAttributeHierarchyArray.js';
-import { getDirectChildren } from '../server-functions/dRow-functions/getDirectChildren.js';
-import { createMenuDataRowsArray } from '../server-functions/dRow-functions/createMenuDataRowsArray';
-import { getAttributeDrows } from '../server-functions/dRow-functions/getAttributeDrows.js';
-import { createCategorisedDataRowsArray } from '../server-functions/menu-server-functions/update-menu-data-row/createCategorisedDataRowsArray.js';
-import { saveMenuDataRows } from '../server-functions/menu-server-functions/update-menu-data-row/saveMenuDataRows.js';
-import { getPendingAttributeDrows } from '../server-functions/dRow-functions/getPendingAttributeDrows.js';
-import { getLastDescriptionWord } from '../server-functions/dRow-functions/getLastDescriptionWord.js';
-import { updatePendingAttributeDrows } from '../server-functions/dRow-functions/updatePendingAttributeDrows.js';
+import { updateMenuDataItem } from '../server-functions/menu-server-functions/update-menu-data-item/updateMenuDataItem.js';
 
 export const updateMenuDataItemMethod = new ValidatedMethod({
   name: "updateMenuDataItemMethod",
@@ -27,28 +12,8 @@ export const updateMenuDataItemMethod = new ValidatedMethod({
     description: {type: String}
   }).validator(),
   run(menuDataItem) {
-		
-		const sourceDrowId = menuDataItem.sourceDrowId;
-		// console.log("update started");
-	
-		createInitialArgs(MENUDATAITEMS, attributePhrases, getDirectChildren, MENUDATAROWS)
-		.then(function(args) {
-			// console.log(args);
-			return getHeadDrow(args, sourceDrowId, menuDataItem);
-		})
-		.then(function(args) {
-			// console.log(args.originalHeadDrow);
-			return getAttributeDrows(args);
-		})
-		.then(function(args) {
-			return getPendingAttributeDrows(args, getLastDescriptionWord);
-		})
-		.then(function(args) {
-			updatePendingAttributeDrows(args, MENUDATAITEMS);
-		})
-		.catch(function(err) {
-			console.log(err);
-		});
+
+  	updateMenuDataItem(menuDataItem);
 	
 	}
 });
